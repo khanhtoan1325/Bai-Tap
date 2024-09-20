d2b7054fb8fd84a201fa48e0e7b33203627ff5cc
KhanhToan <nguyenkhanhtoan1325@gmail.com>
2024-09-20 22:23:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Winbai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTong = new System.Windows.Forms.Button();
+            this.btnHieu = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.lblSoA = new System.Windows.Forms.Label();
+            this.lblSoB = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTong
+            // 
+            this.btnTong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnTong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTong.Location = new System.Drawing.Point(80, 286);
+            this.btnTong.Name = "btnTong";
+            this.btnTong.Size = new System.Drawing.Size(91, 33);
+            this.btnTong.TabIndex = 3;
+            this.btnTong.Text = "&Tổng ";
+            this.btnTong.UseVisualStyleBackColor = false;
+            this.btnTong.Click += new System.EventHandler(this.btnTong_Click);
+            // 
+            // btnHieu
+            // 
+            this.btnHieu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnHieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHieu.Location = new System.Drawing.Point(242, 286);
+            this.btnHieu.Name = "btnHieu";
+            this.btnHieu.Size = new System.Drawing.Size(91, 33);
+            this.btnHieu.TabIndex = 4;
+            this.btnHieu.Text = "&Hiệu";
+            this.btnHieu.UseVisualStyleBackColor = false;
+            this.btnHieu.Click += new System.EventHandler(this.btnHieu_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(409, 286);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(91, 33);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblKetQua.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.Location = new System.Drawing.Point(0, 0);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(576, 88);
+            this.lblKetQua.TabIndex = 0;
+            this.lblKetQua.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSoA
+            // 
+            this.lblSoA.AutoSize = true;
+            this.lblSoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoA.Location = new System.Drawing.Point(75, 143);
+            this.lblSoA.Name = "lblSoA";
+            this.lblSoA.Size = new System.Drawing.Size(110, 18);
+            this.lblSoA.TabIndex = 0;
+            this.lblSoA.Text = "Số Nguyên A ";
+            // 
+            // lblSoB
+            // 
+            this.lblSoB.AutoSize = true;
+            this.lblSoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoB.Location = new System.Drawing.Point(75, 176);
+            this.lblSoB.Name = "lblSoB";
+            this.lblSoB.Size = new System.Drawing.Size(106, 18);
+            this.lblSoB.TabIndex = 0;
+            this.lblSoB.Text = "Số Nguyên B";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(221, 143);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(264, 22);
+            this.txtA.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(221, 176);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(264, 22);
+            this.txtB.TabIndex = 2;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(242, 359);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(91, 33);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Th&oát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnThoat;
+            this.ClientSize = new System.Drawing.Size(576, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblSoB);
+            this.Controls.Add(this.lblSoA);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnHieu);
+            this.Controls.Add(this.btnTong);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tính Tổng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTong;
+        private System.Windows.Forms.Button btnHieu;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblKetQua;
+        private System.Windows.Forms.Label lblSoA;
+        private System.Windows.Forms.Label lblSoB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winbai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            int tong;
+            Console.WriteLine(txtA.Text);
+            Console.WriteLine(txtB.Text);
+            tong = int.Parse(txtA.Text)+ int.Parse(txtB.Text);
+            lblKetQua.Text = tong + ""; 
+        }
+
+        private void btnHieu_Click(object sender, EventArgs e)
+        {
+            int tong;
+            Console.WriteLine(txtA.Text);
+            Console.WriteLine(txtB.Text);
+            tong = int.Parse(txtA.Text) - int.Parse(txtB.Text);
+            lblKetQua.Text = tong + "";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblKetQua.Text = " ";
+            txtA.Text = "";
+            txtB.Text = "";
+        }
+    }
+}
